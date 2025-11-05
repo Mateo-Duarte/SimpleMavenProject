638cc428b5572b67b22a81500c8df07079198a88
Mateo-Duarte <mateo.alvarezd@unac.edu.co>
2025-11-04 21:04:16 -0500

    4 test

--- SimpleMavenProject/src/test/java/edu/unac/CalculatorTest.java
+++ SimpleMavenProject/src/test/java/edu/unac/CalculatorTest.java
@@ -5,12 +5,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(3, 2));
+    }
 
     @Test
-    Void testAdd(){
+    void testDivision(){
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.division(6, 3));
 
+    }
+
+    @Test
+    void testmultiply(){
+        Calculator calc = new Calculator();
+        assertEquals(25, calc.multiply(5, 5));
+
+    }
+
+    @Test
+    void subtract(){
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.subtract(8, 2));
     }
 
 
 
+
+
 }
+
+
+
